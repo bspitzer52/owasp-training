e86d3249e7f47ad575649ada27802f51e9c3acf3
Brandon <81323392+bspitzer52@users.noreply.github.com>
2025-01-01 14:52:03 -0500

    upload payload

--- xss/fuzzy.py
+++ xss/fuzzy.py
@@ -3,87 +3,9 @@ from bs4 import BeautifulSoup
 from termcolor import colored
 from time import time
 
-def how_many_forms(url):
-    print("Fetching Forms List Please Wait..")
-    links = links_to_page(url)
-    number_of_forms = {}
-    for i in links:
-        r = requests.get(i)
-        soup = BeautifulSoup(r.text, "html.parser")
-        form = soup.find_all("form")
-        number_of_forms.update({i: len(form)})
-    return number_of_forms
+# Other functions (how_many_forms, links_to_page, get_inputs, etc.) remain unchanged.
 
-def links_to_page(url):
-    set_for_links = set()
-    try:
-            if ("https://" not in url and "http://" not in url):
-                r = requests.get("http://{}".format(url))
-                first_url = "http://{}".format(url)
-            else:
-                first_url = url
-                r = requests.get(url)
-    except Exception as e:
-        print(e)
-        pass
-    soup = BeautifulSoup(r.text, "html.parser")
-    links = soup.find_all("a")
-
-    if ("https://" in url or "http://" in url):
-        url = url.split("//")[1]
-        if ("www." in url):
-            url = url.split("www.")[1]
-        if ("/" in url):
-            url = url.split("/")[0]
-
-    for i in links:
-        actual_url = i.get("href")
-        url_in_tag = str(i.get("href"))
-        if ("https://" in url_in_tag or "http://" in url_in_tag):
-            url_in_tag = url_in_tag.split("//")[1]
-        if ("www." in url_in_tag):
-            url_in_tag = url_in_tag.split("www.")[1]
-        if ("/" in url_in_tag):
-            url_in_tag = url_in_tag.split("/")[0]
-        if (url in url_in_tag):
-            set_for_links.add(actual_url)
-
-        try:
-            if (url_in_tag[0] == "."):
-                actual_url = first_url + url_in_tag[1:]
-                set_for_links.add(actual_url)
-            elif(url_in_tag[0] != "/" and url[-1] != "/"):
-                actual_url = first_url + "/" + url_in_tag
-                set_for_links.add(actual_url)
-            elif(url_in_tag[0] == "/" and url[-1] == "/"):
-                actual_url = first_url + url_in_tag[1:]
-                set_for_links.add(actual_url)
-            else:
-                actual_url = first_url + url_in_tag
-                set_for_links.add(actual_url)
-        except:
-            pass
-
-    return set_for_links
-
-def get_inputs(url):
-
-    try:
-            if ("https://" not in url and "http://" not in url):
-                r = requests.get("http://{}".format(url))
-            else:
-                r = requests.get(url)
-    except Exception as e:
-        print(e)
-        pass
-
-    soup = BeautifulSoup(r.content, "html.parser")
-    list_of_inputs = []
-    for i in soup.find_all("input"):
-        list_of_inputs.append(i)
-
-    return list_of_inputs
-def find_xss(url):
+def find_xss(url, payload):
     xss_flag = False
     try:
         if ("https://" not in url and "http://" not in url):
@@ -94,8 +16,8 @@ def find_xss(url):
 
     print(colored("Request Sent to Site {}".format(url), "green"))
 
-    inputs = get_inputs(url) #Get input boxes
-    if(len(inputs) == 0):
+    inputs = get_inputs(url)  # Get input boxes
+    if len(inputs) == 0:
         return -1
     print(colored("Finding input box in the page..", "green"))
     start = time()
@@ -104,28 +26,28 @@ def find_xss(url):
             for i in inputs:
                 name = str(i.get("name"))
                 r = requests.get(url + "?" + name + "=" + pyld)
-                if(pyld in str(r.content,"utf-8")):
+                if pyld in str(r.content, "utf-8"):
                     xss_flag = True
 
                     print()
-                    print(colored("#FOUND -> Payload:{}".format(pyld),"green"))
+                    print(colored("#FOUND -> Payload:{}".format(pyld), "green"))
                     end = time()
                     ctime = end - start
-                    print(colored("Vulnerable URL -> {}".format(url),"red"))
-                    print(colored("Vulnerable Input Box-> {}".format(i),"red"))
-                    if(xss_flag):
+                    print(colored("Vulnerable URL -> {}".format(url), "red"))
+                    print(colored("Vulnerable Input Box-> {}".format(i), "red"))
+                    if xss_flag:
                         print()
                         print(colored("Time:{} Seconds".format(ctime), "white"))
                         return 1
                 else:
-                    print(colored("#NOT FOUND -> Payload:{}".format(pyld),"red"))
+                    print(colored("#NOT FOUND -> Payload:{}".format(pyld), "red"))
         except Exception as e:
             print(e)
             pass
 
 
 def payloads(file):
-    with open(file,"rb") as f:
+    with open(file, "rb") as f:
         payloads = f.read().splitlines()
     return payloads
 
@@ -135,7 +57,7 @@ if __name__ == '__main__':
         #################################################################
         #                                                               #
         #                       XSS Scraper                             #
-        #               Developed by Chaitanya Dande                    #
+        #               Developed by Brandon Spitzer                    #
         #                                                               #
         #################################################################
     '''
@@ -153,31 +75,28 @@ if __name__ == '__main__':
         print(choices)
         choice = input("Make a Choice -->")
 
-        if (choice == "1"):
+        # Define the payload file directly
+        payload_file = "payload.txt"  # File is in the same directory
+        payload = payloads(payload_file)  # Read payloads from payload.txt
+
+        if choice == "1":
             url = input("Please input the url -> ")
-            file = input("Please input Payloads file name -> ")
-            payload = payloads(file)
-            result = find_xss(url)
-            if (result == -1):
+            result = find_xss(url, payload)
+            if result == -1:
                 print("No input box found to attack.")
             else:
                 print(colored(40 * "-", "red"))
-        elif (choice == "2"):
+        elif choice == "2":
             url = input("Please input the url -> ")
-            file = input("Payloads' file name -> ")
-            payload = payloads(file)
             links = links_to_page(url)
             for link in links:
                 try:
-                    result = find_xss(link)
-                    if(result == -1):
+                    result = find_xss(link, payload)
+                    if result == -1:
                         print("No input box found to attack.")
-                        if (test == "0"):
-                            exit(0)
-                        print(colored(40 * "-", "red"))
-                    elif(result == 1):
+                    elif result == 1:
                         test = input("Do you want to test other links in the page:(1->Continue or 0->Stop) (Default:1)") or "1"
-                        if(test == "0"):
+                        if test == "0":
                             exit(0)
                         print(colored(40 * "-", "red"))
                     else:
@@ -185,17 +104,17 @@ if __name__ == '__main__':
                 except Exception as e:
                     print(e)
                     pass
-        elif (choice == "3"):
+        elif choice == "3":
             url = input("URL Please -> ")
             links = links_to_page(url)
-            for i in links: 
+            for i in links:
                 print(i)
-        elif (choice == "4"):
+        elif choice == "4":
             url = input("URL Please -> ")
             forms = how_many_forms(url)
             for i, j in forms.items():
                 print(i, "->", j, "form(s)")
-        elif (choice == "5"):
+        elif choice == "5":
             url = input("URL Please -> ")
             inputs = get_inputs(url)
             for i in inputs:
