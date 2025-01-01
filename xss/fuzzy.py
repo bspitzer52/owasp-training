5729c2b7639f83244d4175eacbb184d729e91f37
Brandon <81323392+bspitzer52@users.noreply.github.com>
2025-01-01 16:35:42 -0500

    update

--- xss/fuzzy.py
+++ xss/fuzzy.py
@@ -1,25 +1,16 @@
-#!/usr/bin/env python
-
-import argparse
 from scrapy.cmdline import execute
-from xsscrapy.spiders.xss_spider import XSSspider
 import sys
-
-__author__ = 'Dan McInerney'
-__license__ = 'BSD'
-__version__ = '1.0'
-__email__ = 'danhmcinerney@gmail.com'
+import argparse
 
 def get_args():
-    parser = argparse.ArgumentParser(description=__doc__,
-                                    formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-u', '--url', help="URL to scan; -u http://example.com")
-    parser.add_argument('-l', '--login', help="Login name; -l danmcinerney")
-    parser.add_argument('-p', '--password', help="Password; -p pa$$w0rd")
-    parser.add_argument('-c', '--connections', default='30', help="Set the max number of simultaneous connections allowed, default=30")
-    parser.add_argument('-r', '--ratelimit', default='0', help="Rate in requests per minute, default=0")
+    parser = argparse.ArgumentParser(description="XSS Scrapy scanner")
+    parser.add_argument('-u', '--url', help="URL to scan; -u http://example.com", required=True)
+    parser.add_argument('-l', '--login', help="Login name; -l username")
+    parser.add_argument('-p', '--password', help="Password; -p password")
+    parser.add_argument('-c', '--connections', default='30', help="Max simultaneous connections (default=30)")
+    parser.add_argument('-r', '--ratelimit', default='0', help="Rate limit (requests per minute)")
     parser.add_argument('--basic', help="Use HTTP Basic Auth to login", action="store_true")
-    parser.add_argument('-k', '--cookie',help="Cookie key; --cookie SessionID=afgh3193e9103bca9318031bcdf")
+    parser.add_argument('-k', '--cookie', help="Cookie key (e.g., SessionID=afgh3193)")
     args = parser.parse_args()
     return args
 
@@ -31,13 +22,19 @@ def main():
     try:
         cookie_key = args.cookie.split('=',1)[0] if args.cookie else None
         cookie_value = ''.join(args.cookie.split('=',1)[1:]) if args.cookie else None
-        execute(['scrapy', 'crawl', 'xsscrapy',
-                 '-a', 'url=%s' % args.url, '-a', 'user=%s' % args.login, '-a',
-                 'pw=%s' % args.password, '-a', 'basic=%s' % args.basic,
-                 '-a', 'cookie_key=%s' % cookie_key, '-a', 'cookie_value=%s' % cookie_value,
-                 '-s', 'CONCURRENT_REQUESTS=%s' % args.connections,
-                 '-s', 'DOWNLOAD_DELAY=%s' % rate])
+        execute([
+            'scrapy', 'crawl', 'xsscrapy',
+            '-a', f'url={args.url}',
+            '-a', f'user={args.login}',
+            '-a', f'pw={args.password}',
+            '-a', f'basic={args.basic}',
+            '-a', f'cookie_key={cookie_key}',
+            '-a', f'cookie_value={cookie_value}',
+            '-s', f'CONCURRENT_REQUESTS={args.connections}',
+            '-s', f'DOWNLOAD_DELAY={rate}'
+        ])
     except KeyboardInterrupt:
         sys.exit()
 
-main()
+if __name__ == '__main__':
+    main()
