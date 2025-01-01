a665016381ed967c4c427394294f9713120f7142
Brandon <81323392+bspitzer52@users.noreply.github.com>
2025-01-01 17:33:26 -0500

    update

---
+++ xss/README.md
@@ -0,0 +1,48 @@
+<p align="center">
+ <img src="images/logo.png" height="200"><br/>
+A powerful XSS scanner made in python 3.7<br/>
+
+
+## Installing
+
+Requirements: <br/>
+
+<li> BeautifulSoup4 </li>
+
+```bash
+pip install bs4
+```
+<li> requests </li>
+
+```bash
+pip install requests
+```
+<li> python 3.7 </li>
+<br/>
+Commands:
+
+```bash
+python3 fuzzy.py --help 
+```
+## Usage
+Basic usage:
+
+```bash
+python3 fuzzy.py -u http://testphp.vulnweb.com
+```
+<br/>
+Advanced usage:
+
+```bash
+python3 fuzzy.py --help
+```
+
+## Main features
+
+* crawling all links on a website ( crawler engine )
+* POST and GET forms are supported
+* many settings that can be customized
+* Advanced error handling
+* Multiprocessing support.✔️
+* ETC....
+
--- xss/fuzzy.py
+++ xss/fuzzy.py
@@ -1,40 +1,73 @@
-from scrapy.cmdline import execute
-import sys
+'''
+XSSCon - 2019/2020
+This project was created by menkrep1337 with 407Aex team. 
+Copyright under the MIT license
+'''
 import argparse
-
-def get_args():
-    parser = argparse.ArgumentParser(description="XSS Scrapy scanner")
-    parser.add_argument('-u', '--url', help="URL to scan; -u http://example.com", required=True)
-    parser.add_argument('-l', '--login', help="Login name; -l username")
-    parser.add_argument('-p', '--password', help="Password; -p password")
-    parser.add_argument('-c', '--connections', default='30', help="Max simultaneous connections (default=30)")
-    parser.add_argument('-r', '--ratelimit', default='0', help="Rate limit (requests per minute)")
-    parser.add_argument('--basic', help="Use HTTP Basic Auth to login", action="store_true")
-    parser.add_argument('-k', '--cookie', help="Cookie key (e.g., SessionID=afgh3193)")
-    args = parser.parse_args()
-    return args
-
-def main():
-    args = get_args()
-    rate = args.ratelimit
-    if rate not in [None, '0']:
-        rate = str(60 / float(rate))
-    try:
-        cookie_key = args.cookie.split('=',1)[0] if args.cookie else None
-        cookie_value = ''.join(args.cookie.split('=',1)[1:]) if args.cookie else None
-        execute([
-            'scrapy', 'crawl', 'xsscrapy',
-            '-a', f'url={args.url}',
-            '-a', f'user={args.login}',
-            '-a', f'pw={args.password}',
-            '-a', f'basic={args.basic}',
-            '-a', f'cookie_key={cookie_key}',
-            '-a', f'cookie_value={cookie_value}',
-            '-s', f'CONCURRENT_REQUESTS={args.connections}',
-            '-s', f'DOWNLOAD_DELAY={rate}'
-        ])
-    except KeyboardInterrupt:
-        sys.exit()
-
-if __name__ == '__main__':
-    main()
+from lib.helper.helper import *
+from lib.helper.Log import *
+from lib.core import *
+from random import randint
+from lib.crawler.crawler import *
+epilog="""
+Github: https://www.github.com/menkrep1337/XSSCon
+Version: 0.5 Final
+"""
+def check(getopt):
+	payload=int(getopt.payload_level)
+	if payload > 6 and getopt.payload is None:
+		Log.info("Do you want use custom payload (Y/n)?")
+		answer=input("> "+W)
+		if answer.lower().strip() == "y":
+			Log.info("Write the XSS payload below")
+			payload=input("> "+W)
+		else:
+			payload=core.generate(randint(1,6))
+	
+	else:
+		payload=core.generate(payload)
+			
+	return payload if getopt.payload is None else getopt.payload
+	
+def start():
+	parse=argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,usage="XSSCon -u <target> [options]",epilog=epilog,add_help=False)
+	
+	pos_opt=parse.add_argument_group("Options")
+	pos_opt.add_argument("--help",action="store_true",default=False,help="Show usage and help parameters")
+	pos_opt.add_argument("-u",metavar="",help="Target url (e.g. http://testphp.vulnweb.com)")
+	pos_opt.add_argument("--depth",metavar="",help="Depth web page to crawl. Default: 2",default=2)
+	pos_opt.add_argument("--payload-level",metavar="",help="Level for payload Generator, 7 for custom payload. {1...6}. Default: 6",default=6)
+	pos_opt.add_argument("--payload",metavar="",help="Load custom payload directly (e.g. <script>alert(2005)</script>)",default=None)
+	pos_opt.add_argument("--method",metavar="",help="Method setting(s): \n\t0: GET\n\t1: POST\n\t2: GET and POST (default)",default=2,type=int)
+	pos_opt.add_argument("--user-agent",metavar="",help="Request user agent (e.g. Chrome/2.1.1/...)",default=agent)
+	pos_opt.add_argument("--single",metavar="",help="Single scan. No crawling just one address")
+	pos_opt.add_argument("--proxy",default=None,metavar="",help="Set proxy (e.g. {'https':'https://10.10.1.10:1080'})")
+	pos_opt.add_argument("--about",action="store_true",help="Print information about XSSCon tool")
+	pos_opt.add_argument("--cookie",help="Set cookie (e.g {'ID':'1094200543'})",default='''{"ID":"1094200543"}''',metavar="")
+	
+	getopt=parse.parse_args()
+	print(logo)
+	Log.info("Starting XSSCon...")
+	if getopt.u:
+		core.main(getopt.u,getopt.proxy,getopt.user_agent,check(getopt),getopt.cookie,getopt.method)
+		
+		crawler.crawl(getopt.u,int(getopt.depth),getopt.proxy,getopt.user_agent,check(getopt),getopt.method,getopt.cookie)
+		
+	elif getopt.single:
+		core.main(getopt.single,getopt.proxy,getopt.user_agent,check(getopt),getopt.cookie,getopt.method)
+		
+	elif getopt.about:
+		print("""
+***************
+Project: XSSCon
+License: MIT
+Author: menkrep1337
+Last updates: 2019 may 26
+Note: Take your own RISK
+****************
+"""+epilog)
+	else:
+		parse.print_help()
+		
+if __name__=="__main__":
+	start()
